d916a9c3d8a80f8f84006e3334c88075666be086
stanny880913 <stanny11122@gmail.com>
2023-03-15 14:25:31 +0800

    clean code

--- medium/leetcode958.cpp
+++ medium/leetcode958.cpp
@@ -5,28 +5,6 @@
 
 using namespace std;
 
-// struct TreeNode
-// {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-// };
-
-// TreeNode *buildTree(vector<int> &arr, int index)
-// {
-//     TreeNode *root = nullptr;
-//     if (index < arr.size() && arr[index] != NULL)
-//     {
-//         root = new TreeNode(arr[index]);
-//         root->left = buildTree(arr, 2 * index + 1);
-//         root->right = buildTree(arr, 2 * index + 2);
-//     }
-//     return root;
-// }
-
 bool isCompleteTree(TreeNode *root)
 {
     if (root == NULL)
@@ -62,17 +40,9 @@ bool isCompleteTree(TreeNode *root)
 
 int main()
 {
-    // TreeNode *root = new TreeNode(1);
-    // root->right = new TreeNode(3);
-    // root->left = new TreeNode(2);
-    // root->left->right = new TreeNode(5);
-    // root->left->left = new TreeNode(4);
-    // root->right->left = new TreeNode(6);
-    // root->right->right = new TreeNode(7);
-
     vector<int> tree_arr = {1, 2, 3, 4, 5, NULL, 7};
     int index = 0;
     // buildTree(tree_arr, 0);
-    isCompleteTree(buildTree(tree_arr,0));
+    isCompleteTree(buildTree(tree_arr, 0));
     return 0;
 }
