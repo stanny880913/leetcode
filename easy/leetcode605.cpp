5821ce44c5690ed7860146c8f062b55bc4d189f2
stanny880913 <stanny11122@gmail.com>
2023-03-21 13:20:31 +0800

    leetcode605

--- easy/leetcode605.cpp
+++ easy/leetcode605.cpp
@@ -21,7 +21,7 @@ bool canPlaceFlowers(vector<int> &flowerbed, int n)
 
         if (flowerbed[i] == 0)
         {
-            flowerbed[i] = 1;
+            flowerbed[i] == 1;
             --n;
         }
         if (n <= 0)
