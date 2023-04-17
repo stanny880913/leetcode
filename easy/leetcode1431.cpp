aa565748791dd8c24a500214806e91e3e7b158b0
stanny880913 <stanny11122@gmail.com>
2023-04-17 13:26:39 +0800

    leetcode1431

---
+++ easy/leetcode1431.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+{
+    vector<bool> ans;
+    int max = *max_element(candies.begin(), candies.end());
+
+    for (int i = 0; i < candies.size(); i++)
+    {
+        if(candies[i]+extraCandies >= max)
+            ans.push_back(true);
+        else
+            ans.push_back(false);
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> candies = {2, 3, 5, 1, 3};
+    int extraCandies = 3;
+    kidsWithCandies(candies, extraCandies);
+    return 0;
+}
