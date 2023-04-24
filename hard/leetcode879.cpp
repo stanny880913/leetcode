7962e7841741602fa6aa2a72d45078f99a1daab3
StannyHo <stanny11122@gmail.com>
2023-04-24 13:03:13 +0800

    leetcode879 unfinished

---
+++ hard/leetcode879.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int profitableSchemes(int n, int minProfit, vector<int> &group, vector<int> &profit)
+{
+    
+}
+
+int main()
+{
+    int n = 5, minProfit = 3;
+    vector<int> group = {2, 2};
+    vector<int> profit = {2, 3};
+    profitableSchemes(n, minProfit, group, profit);
+    return 0;
+}
