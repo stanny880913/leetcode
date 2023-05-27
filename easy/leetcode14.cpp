b8d49c76fc335a5d15b5da255141713b36b770e5
StannyHo <stanny11122@gmail.com>
2023-05-28 04:46:14 +0800

    leetcode14

---
+++ easy/leetcode14.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string longestCommonPrefix(vector<string> &strs)
+{
+    if (strs.empty())
+        return "";
+
+    sort(strs.begin(), strs.end());
+
+    string x = strs[0];
+    string y = strs[strs.size() - 1];
+    string ans = "";
+
+    for (int i = 0; i < x.size(); i++)
+    {
+        if (x[i] == y[i])
+            ans += x[i];
+        else
+            break;
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<string> strs = {{""}};
+    longestCommonPrefix(strs);
+    return 0;
+}
