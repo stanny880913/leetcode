cd38b4b97d24686d34c5661077d3937118a284e6
stanny880913 <stanny11122@gmail.com>
2023-07-04 13:51:51 +0800

    leetcode136

---
+++ easy/leetcode136.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+int singleNumber(vector<int> &nums)
+{
+    map<int, int> map;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        map[nums[i]]++;
+    }
+    for (int j = 0; j < nums.size(); j++)
+    {
+        if(map[nums[j]] == 1)
+            return nums[j];
+    }
+    return 0;
+}
+
+int main()
+{
+    vector<int> nums = {2, 2, 1};
+    singleNumber(nums);
+    return 0;
+}
