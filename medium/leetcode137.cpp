d91e299147e2a005f72d23932b51dd703e68c1ed
stanny880913 <stanny11122@gmail.com>
2023-07-04 14:22:22 +0800

    leetcode137 add bit solution

--- medium/leetcode137.cpp
+++ medium/leetcode137.cpp
@@ -4,17 +4,26 @@ using namespace std;
 
 int singleNumber(vector<int> &nums)
 {
-    map<int, int> map;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        map[nums[i]]++;
-    }
-    for (int j = 0; j < nums.size(); j++)
-    {
-        if (map[nums[j]] == 1)
-            return nums[j];
-    }
-    return 0;
+    // map<int, int> map;
+    // for (int i = 0; i < nums.size(); i++)
+    // {
+    //     map[nums[i]]++;
+    // }
+    // for (int j = 0; j < nums.size(); j++)
+    // {
+    //     if (map[nums[j]] == 1)
+    //         return nums[j];
+    // }
+    // return 0;
+
+    // Bit Manipulation
+        int one = 0, two = 0;
+        for (auto num : nums)
+        {
+            one = (one ^ num) & ~two;
+            two = (two ^ num) & ~one;
+        }
+        return one;
 }
 
 int main()
