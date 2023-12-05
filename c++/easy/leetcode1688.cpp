ad81e5d1901bec26c847c4d06fe4c220b15e34dc
stanny880913 <stanny11122@gmail.com>
2023-12-05 15:04:59 +0800

    leetcode1688

---
+++ c++/easy/leetcode1688.cpp
@@ -0,0 +1,17 @@
+
+int numberOfMatches(int n)
+{
+  if (n == 1)
+    return 0;
+  if (n % 2 == 0)
+    return n / 2 + numberOfMatches(n / 2);
+  else
+    return (n - 1) / 2 + numberOfMatches((n - 1) / 2 + 1);
+}
+
+int mail()
+{
+  int n = 7;
+  numberOfMatches(n);
+  return 0;
+}
